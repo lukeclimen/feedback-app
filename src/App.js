887426832e3546db7b2badb189116e5fc687f9b4
Luke Climenhage <luke.climenhage@gmail.com>
2022-09-25 14:20:46 -0400

    Added delete capabilities

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import { useState } from "react";
 import FeedbackData from "./data/FeedbackData";
+import FeedbackItem from "./components/FeedbackItem";
 
 function App() {
 	const [feedback, setFeedback] = useState(FeedbackData);
 
+	const deleteFeedback = (id) => {
+		if (window.confirm("Are you sure you want to delete this?")) {
+			setFeedback(feedback.filter((item) => item.id !== id));
+		}
+	};
+
 	return (
 		<>
 			<Header />
-			<FeedbackList feedback={feedback} />
+			<FeedbackList
+				feedback={feedback}
+				handleDelete={deleteFeedback}
+			/>
 		</>
 	);
 }
