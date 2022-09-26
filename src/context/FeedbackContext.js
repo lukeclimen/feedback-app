350305041b7248981414d26a1514af30350520cb
Luke Climenhage <luke.climenhage@gmail.com>
2022-09-26 17:32:25 -0400

    Changed the addFeedback function to add to the backend

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,6 +1,4 @@
-import { useEffect } from "react";
-import { createContext, useState } from "react";
-import { v4 as uuidv4 } from "uuid";
+import { createContext, useState, useEffect } from "react";
 
 const FeedbackContext = createContext();
 
@@ -21,9 +19,7 @@ export const FeedbackProvider = ({ children }) => {
 
 	// Fetch feedback data
 	const fetchFeedback = async () => {
-		const response = await fetch(
-			"http://localhost:5000/feedback?_sort=id&_order=desc"
-		);
+		const response = await fetch("/feedback?_sort=id&_order=desc");
 		const data = await response.json();
 
 		setFeedback(data);
@@ -38,9 +34,18 @@ export const FeedbackProvider = ({ children }) => {
 	};
 
 	// Add a new feedback item to the feedback list
-	const addFeedback = (newFeedback) => {
-		newFeedback.id = uuidv4();
-		setFeedback([newFeedback, ...feedback]);
+	const addFeedback = async (newFeedback) => {
+		const response = await fetch("/feedback", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(newFeedback),
+		});
+
+		const data = await response.json();
+
+		setFeedback([data, ...feedback]);
 	};
 
 	// Update feedback item
